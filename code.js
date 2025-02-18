b01626b5ef85c2630f2a9c62e1532e1c2f3570b3
Kane Kriz <kanemkriz@gmail.com>
2025-02-18 08:12:01 -0700

    Update code.js

--- code.js
+++ code.js
@@ -6,46 +6,44 @@
 //
 
 
-//
+// WIP, not 100% functional
 
 
 function quicksort(array) //iterative as specified, no recursion
 {
    // var pivot; //probably useless to declare this here, may have to live within the helper
-   var arrLen = array.length;
-   var pivotIndex;
-   var stack;
-   
-   if (arrLen == 0 || arrLen == 1)
+    var arrLen = array.length;
+
+   if(arrLen == 0 || arrLen == 1)
     {
         return array; //0 or 1 elements, no need to sort, just return
     }
 
-   stack = [[0, arrLen - 1]]; //tried to implement this w/o a stack, maybe only option?
+    var startPos = 0;
+    var endPos = arrLen - 1;
+    var pivotIndex = partitionArr(array, startPos, endPos);
 
-    while(stack.length > 0)
-       {
-         var popped = stack.pop();
-         var startPos = popped[0];
-         var endPos = popped[1];
-          
-        if(startPos < endPos) 
+    while(true)
+    {
+        if(startPos < pivotIndex - 1)
         {
+            endPos = pivotIndex - 1;
             pivotIndex = partitionArr(array, startPos, endPos);
-
-            if(pivotIndex - 1 > startPos)
-            {
-                stack.push([startPos, pivotIndex - 1]);
-            }
-
-            if(pivotIndex + 1 < endPos) 
-            {
-                stack.push([pivotIndex + 1, endPos]);
-            }
         }
+            
+        else if(endPos > pivotIndex + 1)
+        {
+            startPos = pivotIndex + 1;
+            pivotIndex = partitionArr(array, startPos, endPos);
+        }
+            
+        else
+        {
+            break; //exit while true via break once neither the if or else if is satisfied
+        }
+    
     }
-
-    return array;
+   return array;
 }
 
 
@@ -61,30 +59,15 @@ function partitionArr(array, startPos, endPos)
 
         pivot = array[endPos]; //select pivot element
         leftIndex = startPos - 1; //set leftIndex to position one before the start of the selection
-        var i = startPos;
 
-        while(i < endPos) //iterate over elements from startPos to endPos-1, all positions within the array as desired
+        for(var j = startPos; j < endPos; j++) //iterate over elements from startPos to endPos-1, all positions within the array as desired
         {
-            if(array[i] < pivot) 
+            if(array[j] <= pivot) 
             {
                 leftIndex++; //move leftIndex forward to adjust
-                temp = array[leftIndex]; //swap elements array[leftIndex] and array[i] using temp var
-                array[leftIndex] = array[i];
-                array[i] = temp;
-                i++;
-            }
-               
-            else if(array[i] === pivot)
-            {
-                i++;
-            }
-               
-            else
-            {
-                temp = array[i];
-                array[i] = array[endPos - 1];
-                array[endPos - 1] = temp;
-                endPos--;
+                temp = array[leftIndex]; //swap elements array[leftIndex] and array[j] using temp var
+                array[leftIndex] = array[j];
+                array[j] = temp;
             }
         }
 
@@ -92,5 +75,5 @@ function partitionArr(array, startPos, endPos)
         array[leftIndex + 1] = array[endPos];
         array[endPos] = temp;
 
-        return leftIndex + 1;
+        return leftIndex + 1; //may need to change
 }
